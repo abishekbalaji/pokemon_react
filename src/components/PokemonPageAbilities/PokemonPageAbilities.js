5ae0d5ae8d0aa07aa6d973f929ff6d918f125fe2
Abishek Balaji <abishekbalaji7.151@gmail.com>
2023-02-12 11:39:08 +0530

    Reduced images size and added keys for map

--- src/components/PokemonPageAbilities/PokemonPageAbilities.js
+++ src/components/PokemonPageAbilities/PokemonPageAbilities.js
@@ -4,10 +4,8 @@ const PokemonPageAbilities = ({ pokemon }) => {
   return (
     <div className="pokemon-page_abilities-container">
       {pokemon.abilities.map((obj, idx) => (
-        <div className="pokemon-page_ability-container">
-          <span className="pokemon-page_ability-name" key={idx}>
-            {obj.ability.name}
-          </span>
+        <div key={idx} className="pokemon-page_ability-container">
+          <span className="pokemon-page_ability-name">{obj.ability.name}</span>
         </div>
       ))}
     </div>
--- src/components/PokemonPageStats/PokemonPageStats.js
+++ src/components/PokemonPageStats/PokemonPageStats.js
@@ -3,8 +3,8 @@ import "./PokemonPageStats.scss";
 const PokemonPageStats = ({ pokemon }) => {
   return (
     <div className="pokemon-page_stats-container">
-      {pokemon.stats.map((stat) => (
-        <div className="pokemon-page_stats-stat-container">
+      {pokemon.stats.map((stat, idx) => (
+        <div key={idx} className="pokemon-page_stats-stat-container">
           <span className="pokemon-page_stats-stat-name">{stat.stat.name}</span>
           <span className="pokemon-page_stats-stat-value">
             {stat.base_stat}
